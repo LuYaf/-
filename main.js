c17f9360264e9fea41f2bd961203af22bf28004c
yafang <3135856680@qq.com>
2023-04-27 16:26:59 +0800

    更正错误

--- main.js
+++ main.js
@@ -18,7 +18,6 @@ import { bbox } from 'ol/loadingstrategy';
 import { Vector } from 'ol/layer';
 
 
-
 // const container = document.getElementById('popup');
 // const content = document.getElementById('popup-content');
 // const closer = document.getElementById('popup-closer');
@@ -43,7 +42,7 @@ import { Vector } from 'ol/layer';
 const map = new Map({
   target: 'map',
   view: new View({
-    center: fromLonLat([75.83379993601642, 26.538130654400197]),
+    center: [12153494.776357276, 4076801.3198558404],//这个位置是西安
     zoom: 3.5,
   }),
 });
@@ -98,7 +97,7 @@ const tileMark2 = new TileLayer({
 })
 
 const baseLayerGroup = new LayerGroup({
-  layers: [
+  layers:[
     openStreetMapstandard, stamenTerrain, ordinaryMap, terrainMap, tileMark1, tileMark2
   ]
 })
@@ -106,13 +105,10 @@ map.addLayer(baseLayerGroup);
 
 //地图切换功能
 const baseLayerElements = document.querySelectorAll('.sidebar >input[type=radio]')
-// console.log(baseLayerElements);
-for (let baseLayerElement of baseLayerElements) {
-  // console.log(baseLayerElements)
-  baseLayerElement.addEventListener('change', function () {
-    // console.log(this.value)
+for(let baseLayerElement of baseLayerElements){
+  baseLayerElement.addEventListener('change', function(){
     let baseLayerElementValue = this.value;
-    baseLayerGroup.getLayers().forEach(function (element, index, array) {
+    baseLayerGroup.getLayers().forEach(function(element, index, array){
       let baseLayerTitle = element.get('title');
       element.setVisible(baseLayerTitle === baseLayerElementValue);
     })
@@ -126,7 +122,7 @@ const stroke = new Stroke({
   color: '#3399CC',
   width: 1.25,
 });
-const xinjiangstyles = [
+const styles = [
   new Style({
     image: new Circle({
       fill: fill,
@@ -145,36 +141,90 @@ const xinjiangGeoJSON = new VectorImageLayer({
   }),
   visible: false,
   title: 'xinjiangGeoJSON',
-  style: xinjiangstyles
-})
+  style: styles
+  })
 map.addLayer(xinjiangGeoJSON);
+
+//点击菜单显示新疆地图
+const grasslandMenu = document.getElementById('grassland');
+const grasslandSubMenu = document.querySelectorAll('#grassland + ul > li')[1]; // 选择多种资源子菜单项
+grasslandSubMenu.addEventListener('click', function() {
+  xinjiangGeoJSON.setVisible(true);
+  grasslandRoadGeoJSON.setVisible(false);
+});
+
+//草原丝绸之路路线
+const grasslandRoadGeoJSON = new VectorImageLayer({
+  source: new VectorSource({
+    url: './data/grassland/grasslandRoad.geojson',
+    format: new GeoJSON()
+  }),
+  visible: false,
+  title: 'grasslandRoadGeoJSON',
+  style: styles
+  })
+map.addLayer(grasslandRoadGeoJSON);
+
+//点击菜单触发草原丝绸之路
+const grasslandRoad = document.getElementById('grasslandroad');
+grasslandRoad.addEventListener('click', function() {
+  // 这里设置显示silkRoadGeoJSON图层，隐藏其他图层
+  grasslandRoadGeoJSON.setVisible(true);
+  xinjiangGeoJSON.setVisible(false);
+  // 以此类推，如果有更多的图层，都可以在这里设置隐藏
+});
+
+
+
 
 //新疆地区点击显示json信息
 const overlayContainerinerElement = document.querySelector('.overlay-container');
 const overlayLayer = new Overlay({
-  element: overlayContainerinerElement
+  element: overlayContainerinerElement  
 })
 map.addOverlay(overlayLayer);
-const overlayFratureName = document.getElementById('feature-name');
-const overlayFratureInfo = document.getElementById('feature-info')
 
-map.on('click', function (e) {
+const overlayFratureName = document.getElementById('feature-name');//name
+const overlayFratureInfo = document.getElementById('feature-info')//info
+const overlayFeatureImages = document.getElementById('feature-images');//img
+
+map.on('click', function(e) {
   overlayLayer.setPosition(undefined);
-  map.forEachFeatureAtPixel(e.pixel, function (feature, layer) {
-    let clickedCoodinate = e.coordinate;
-    let clickedFeatureName = (feature.get('name'));
-    //let clickedFeatureInfo = (feature.get('info'));
-    overlayLayer.setPosition(clickedCoodinate);
+  let clickedCoordinate = e.coordinate;
+  let clickedFeature = null;
+  map.forEachFeatureAtPixel(e.pixel, function(feature, layer) {
+    clickedFeature = feature;
+  }, {
+    layerFilter: function(layerCandidate) {
+      return layerCandidate.get('title') === 'xinjiangGeoJSON';
+    }
+  });
+
+  if (clickedFeature) {
+    let clickedFeatureID = clickedFeature.get('id');
+    let clickedFeatureName = clickedFeature.get('name');
+    let clickedFeatureInfo = clickedFeature.get('info');
+    let clickedFeatureImages = clickedFeature.get('images');
+
+    overlayLayer.setPosition(clickedCoordinate);
     overlayFratureName.innerHTML = clickedFeatureName;
     overlayFratureInfo.innerHTML = clickedFeatureInfo;
-  },
-    {
-      layerFilter: function (layerCandidate) {
-        return layerCandidate.get('title') === 'xinjiangGeoJSON'
-      }
-    }
-  )
-})
+    overlayFeatureImages.innerHTML = '';
+
+    clickedFeatureImages.forEach(function(imagePath) {
+      const imgElement = document.createElement('img');
+      imgElement.src = imagePath;
+      imgElement.className = 'feature-image';
+      overlayFeatureImages.appendChild(imgElement);
+
+      imgElement.addEventListener('click', function() {// 点击图片时触发放大操作
+        imgElement.classList.toggle('enlarge');
+      });
+    });
+  }
+});
+
+
 
 // 丝绸之路路线图
 const routestyles = function(feature) {
